7fe00ec4985e2ca77e36b51c7bd29099ba246b18
sacopon <sacopon.dev@gmail.com>
2024-07-30 22:03:30 +0900

    chapter3-4-14

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -1,14 +1,7 @@
-fn incr_by_owned(x: i32) -> i32 {
-    x + 1
-}
-
-fn incr_by_ref(x_ref: &i32) -> i32 {
-    *x_ref + 1
-}
-
 fn main() {
     let x = 1;
-    let x_ref = &x;
-    println!("{}", incr_by_owned(x));
-    println!("{}", incr_by_ref(x_ref));
+    println!("{}", x);
+
+    let x = 2;
+    println!("{}", x);
 }
