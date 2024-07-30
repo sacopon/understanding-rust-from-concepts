626a31f8a65ea735dcdf0e184a21b66b2033b859
sacopon <sacopon.dev@gmail.com>
2024-07-30 21:41:31 +0900

    chapter3-4-8

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -1,16 +1,7 @@
-fn myclear(x: &mut String) {
-    x.clear();
-}
-
 fn main() {
-    let mut s = "Hello".to_string();
-    println!("s={}", s);
+    let mut x = 1;
+    let x_ref = &x;
 
-    let s_ref = &mut s;
-    myclear(s_ref);
-    println!("s={}", s);
-
-    let s_ref2 = &mut s;
-    myclear(s_ref2);
-    println!("s={}", s);
+    x = 2;
+    println!("{}", x_ref);
 }
