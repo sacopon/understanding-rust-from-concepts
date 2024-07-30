ca2c449f46412ddf9c616dff498fc8c9235aacca
sacopon <sacopon.dev@gmail.com>
2024-07-30 14:11:19 +0900

    chapter3-3-3

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -4,6 +4,7 @@ fn myprint<T: std::fmt::Display>(msg: T) {
 
 fn main() {
     let s = "Hello".to_string();
-    myprint(s);
-    // myprint(s); // s をもう1回出力
+    let ss = s.clone(); // s のコピーを ss に作っておく
+    myprint(s); // s の所有権が関数内の変数に移動
+    myprint(ss);    // ss の所有権が関数内の変数に移動
 }
