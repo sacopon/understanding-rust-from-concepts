c12b6981dae0e83734537dabeb9bcd4d97fc99d7
sacopon <sacopon.dev@gmail.com>
2024-07-30 14:15:09 +0900

    chapter3-4-2

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -1,9 +1,13 @@
 fn myprint<T: std::fmt::Display>(msg: &T) {
-    println!("{}", *msg);
+    println!("{}", msg);
 }
 
 fn main() {
     let s = "Hello".to_string();
-    myprint(&s);    // リファレンスによって関数に渡している
-    myprint(&s);    // s が所有権を失わないので 2 回実行できる
+    let s_ref = &s;
+    let s_ref2 = &s;
+    myprint(s_ref);    // リファレンスによって関数に渡している
+    myprint(s_ref);    // s が所有権を失わないので 2 回実行できる
+    myprint(s_ref2);    // リファレンスによって関数に渡している
+    myprint(s_ref2);    // s が所有権を失わないので 2 回実行できる
 }
