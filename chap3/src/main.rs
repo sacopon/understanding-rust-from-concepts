ab3bedd675f50347b22c7ca6fed134cdbe1a229f
sacopon <sacopon.dev@gmail.com>
2024-07-30 21:50:55 +0900

    chapter3-4-11

--- chap3/src/main.rs
+++ chap3/src/main.rs
@@ -1,8 +1,11 @@
+fn pick1(x: &[i32], end: usize) -> &[i32] {
+    &x[..end]
+}
+
 fn main() {
-    let x;
-    {
-        let y = 1;
-        x = &y;
+    let v1 = [1, 2, 3, 4, 5];
+    let p = pick1(&v1, 2);
+    for ss in p {
+        println!("{}", ss);
     }
-    println!("{}", x);
 }
